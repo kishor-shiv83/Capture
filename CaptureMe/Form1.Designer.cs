11d4ddc9c496d07b6947ffdfa6022defe4876c56
kishor-shiv83 <kishor.shiv@gmail.com>
2021-08-11 23:10:39 +0530

    Capture Project Setup

---
+++ CaptureMe/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace CaptureMe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblLabel = new System.Windows.Forms.Label();
+            this.cmContextCaptureMe = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyCaptureMe = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.cmContextCaptureMe.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblLabel
+            // 
+            this.lblLabel.AutoSize = true;
+            this.lblLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLabel.Location = new System.Drawing.Point(149, 14);
+            this.lblLabel.Name = "lblLabel";
+            this.lblLabel.Size = new System.Drawing.Size(92, 20);
+            this.lblLabel.TabIndex = 0;
+            this.lblLabel.Text = "Capture Me";
+            // 
+            // cmContextCaptureMe
+            // 
+            this.cmContextCaptureMe.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.cmContextCaptureMe.Name = "cmContextCaptureMe";
+            this.cmContextCaptureMe.Size = new System.Drawing.Size(104, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyCaptureMe
+            // 
+            this.notifyCaptureMe.ContextMenuStrip = this.cmContextCaptureMe;
+            this.notifyCaptureMe.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyCaptureMe.Icon")));
+            this.notifyCaptureMe.Text = "Settings";
+            this.notifyCaptureMe.Visible = true;
+            this.notifyCaptureMe.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyCaptureMe_MouseDoubleClick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(56, 2);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(87, 45);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnStart.BackgroundImage")));
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(1, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(49, 45);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 125);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblLabel);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Capture Me";
+            this.Move += new System.EventHandler(this.Form1_Move);
+            this.cmContextCaptureMe.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLabel;
+        private System.Windows.Forms.ContextMenuStrip cmContextCaptureMe;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyCaptureMe;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ CaptureMe/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaptureMe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            if(this.WindowState==FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyCaptureMe.ShowBalloonTip(1000,"Capture Me", "Capture Me Application is running to capture your screens at each 10 mins of interval.", ToolTipIcon.Info);
+            }
+        }
+
+        private void notifyCaptureMe_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            var timer = new System.Threading.Timer(
+                ex => SaveImage(),
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromMinutes(2));
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void SaveImage()
+        {
+
+            var currentImage = CaptureScreen();
+            currentImage.Save(string.Format(@"C:\Users\hp\Documents\CaptureImage\ShivKishor_{0}.jpg", DateString()), ImageFormat.Jpeg);
+
+           
+        }
+
+        private string DateString()
+        {
+            return DateTime.Now.ToString("yyyyMMddHHmmss");
+
+        }
+
+
+        private static Image CaptureScreen()
+        {
+            Rectangle screenSize = Screen.PrimaryScreen.Bounds;
+            var target = new Bitmap(screenSize.Width, screenSize.Height);
+            using (Graphics g = Graphics.FromImage(target))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, new Size(screenSize.Width, screenSize.Height));
+            }
+            return target;
+        }
+
+
+    }
+}
